c5a14e63b923a91e2e1cc7fb415c81b591cc7174
tianshuqu <tq4@illinois.edu>
2018-01-30 18:44:34 -0600

    lab2 assignment

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,25 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while (isFinished() == false) {
+                move();
+                turnRight();
+                if (canMove() == true){
+                    continue;
+                } else {
+                    Turn left;
+                    If (can move() == true) {
+                        Continue;
+                    }
+                    Else {
+                        Turn left;
+                        If (canmove() == true) {
+                            Continue;
+                        }
+                        Else turn left;
+                        continue;
+
+                        // Implement your maze solving algorithm here
         }
 
         if (maze.isFinished()) {
